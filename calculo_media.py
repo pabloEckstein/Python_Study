5e96ad00317337c9a8dc5a19bc8e0e0feb6c537b
pabloEckstein <43323587+pabloEckstein@users.noreply.github.com>
2025-01-29 21:49:01 -0300

    Mudança de nome de arquivo

---
+++ calculo_media.py
@@ -0,0 +1,16 @@
+nota_1 = float(input("Digite a primeira nota: "))
+while nota_1 < 0 or nota_1 > 10:
+    nota_1 = float(input("Valores inválidos. Digite Novamente: "))
+
+nota_2 = float(input("Digite a segunda nota: "))
+while nota_1 < 0 or nota_1 > 10:
+    nota_2 = float(input("Valores inválidos. Digite Novamente: "))
+
+media = (nota_1 + nota_2) / 2
+
+if media < 6:
+    print(f"Aluno reprovado com a média {media:.2f}")
+else:
+    print(f"Aluno aprovado com a média {media:.2f}")
+
+print("teste")
