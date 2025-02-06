4b2933b2c3bd2b0fd483e618858551b3afe13213
pabloEckstein <43323587+pabloEckstein@users.noreply.github.com>
2025-02-05 21:26:41 -0300

    Alteração do código para criar um arquivo texto com as informações fornecidas pelo usuário.

--- create_file.py
+++ create_file.py
@@ -1,3 +1,16 @@
-f = open("arquivo_teste.txt", "x")
-f.write("Teste")
+import os
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+email = input("Informe o seu e-mail: ")
+bio = input("Informe algo adicional sobre você: ")
+info = (f"O meu nome é {nome}, tenho {idade} anos. Meu email é {email}. Mais informação sobre mim: \n\n{bio}")
+nome_arquivo = (f"{nome}_info.txt")
+f = open(nome_arquivo, "x")
+f.write(info)
 f.close()
+x = input("Quer apagar o arquivo? (s para SIM, n para NÃO)")
+if x == "s":
+    os.remove(nome_arquivo)
+else:
+    f = open(nome_arquivo)
